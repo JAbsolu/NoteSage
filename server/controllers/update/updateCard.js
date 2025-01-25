fad991106349a39f4e7065ba44a0e9d16449c62a
John Absolu <Johnson.absolu@hotmail.com>
2025-01-25 14:50:24 -0500

    Added controllers for updates

--- server/controllers/update/updateCard.js
+++ server/controllers/update/updateCard.js
@@ -1,24 +1,20 @@
 const Card = require("../../models/Card");
-const Deck = require("../../models/Deck")
 
 const updateCard = async (req, res) => {
   try {
-    const { cid, did } = req.query;
+    const { id } = req.query;
     const { front, back } = req.body;
 
     //validate user and card id
-    const card = await Card.findById(cid);
-    const deck = await Deck.findById(did);
+    const card = await Card.findById(id);
 
     if (!card) return res.status(400).json({ message: "invalid card id" });
-    if (!deck) return res.status(400).json({ message: "invalid deck id" });
 
     //create filter
-    const filter = { _id: cid, deckId: did };
+    const filter = { _id: id };
     const update = { front: front, back: back };
 
     await Card.findOneAndUpdate(filter, update);
-
     const updatedCard = await Card.findOne(filter);
 
     res.status(200).json({ message: "card updated", update: updatedCard });
