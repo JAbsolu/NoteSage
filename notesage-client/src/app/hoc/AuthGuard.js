17305a613ed5c3999323ac24673f7e8eb56cc775
John Absolu <Johnson.absolu@hotmail.com>
2025-02-15 11:46:23 -0500

    Added guard to protect private routes

---
+++ notesage-client/src/app/hoc/AuthGuard.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
+
+const AuthGuard = (WrappedComponent) => {
+  return (props) => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      const token = Cookies.get("auth-token"); // Read the auth cookie
+      if (!token) {
+        router.push("/login"); // Redirect to login if not authenticated
+      } else {
+        setLoading(false);
+      }
+    }, [router]);
+
+    if (loading) return <p>Loading...</p>; // Show loading while redirecting
+
+    return <WrappedComponent {...props} />;
+  };
+};
+
+export default AuthGuard;
