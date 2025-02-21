2d481d8b98bc428584722f6ccae5ef37286d3b91
John Absolu <Johnson.absolu@hotmail.com>
2025-02-21 17:45:04 -0500

    Updated Page

--- notesage-client/src/app/page.jsx
+++ notesage-client/src/app/page.jsx
@@ -28,8 +28,8 @@ const Home = () => {
 
             {/* Buttons */}
             <div className="mt-6 mb-16 flex space-x-6">
-              <Link href="/signup" className="px-8 py-2 bg-blue text-white rounded-full text-lg font-semibold hover:bg-blue-dark transition">
-                Sign up Today
+              <Link href="/signup" className="px-8 py-2 border-2 border-blue bg-blue text-white rounded-full text-lg font-semibold hover:bg-blue-dark transition">
+                Sign up for free
               </Link>
               <Link href="/how-it-works" className="px-8 py-2 border-2 border-blue text-blue rounded-full text-lg font-semibold hover:bg-blue hover:text-white transition">
                 How it Works
