c5692f24495285a1a7d061428289b03988310ccf
John Absolu <Johnson.absolu@hotmail.com>
2025-03-24 12:38:40 -0400

    Added flashcard page

---
+++ notesage-client/src/app/dashboard/flashcards/page.jsx
@@ -0,0 +1,57 @@
+"use client"
+
+import DashboardNavbar from "@/components/DashboardNavbar";
+import Sidebar from "@/components/Sidebar";
+import { getCookie } from "@/util/cookies";
+import { useEffect, useState } from "react";
+
+const FlaschardsPage = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [sidebarExpanded, setSidebarExpanded] = useState(false);
+  const userId = getCookie("userId");
+  const token = getCookie("token");
+
+  useEffect(() =>  {
+    getUser(userId)
+  })
+
+  const getUser = async (id) => {
+    try {
+      const response = await fetch(`http://localhost/user?id=${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": token,
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setFirstName(result.data.firstName);
+        setLastName(result.data.lastName);
+        setEmailAddress(result.data.emailAddress);
+      } else {
+        console.log(response.status, response.statusText, "Error fetching user");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  return (
+    <div>
+       <DashboardNavbar
+        toggleSidebar={() => setSidebarExpanded(!sidebarExpanded)}
+        firstName={firstName}
+        lastName={lastName}
+        emailAddress={emailAddress}
+      />
+      <Sidebar isExpanded={sidebarExpanded} />
+    </div>
+  )
+}
+
+export default FlaschardsPage;
