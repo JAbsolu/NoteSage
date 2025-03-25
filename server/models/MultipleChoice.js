75b929e6fa8fbc3619a51b8c6e1158666e373911
John Absolu <Johnson.absolu@hotmail.com>
2025-03-24 21:57:39 -0400

    Added userIds to all models

--- server/models/MultipleChoice.js
+++ server/models/MultipleChoice.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require("mongoose");
 
 const MultipleChoiceSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   testId: { type: mongoose.Schema.Types.ObjectId, ref: "Test" },
   question: {
     type: String,
