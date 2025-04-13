b0e47e1b4c609262424c9457ed9fd389a6c2e569
John Absolu <90818638+JAbsolu@users.noreply.github.com>
2025-04-13 00:49:45 -0400

    Update next.config.mjs

--- notesage-client/next.config.mjs
+++ notesage-client/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_OPENAI_API_KEY: process.env["NEXT_PUBLIC_OPENAI_API_KEY"],
+        NEXT_PUBLIC_OPENAI_ORG_ID: process.env["NEXT_PUBLIC_OPENAI_ORG_ID"],
+        NEXT_PUBLIC_OPENAI_PROJECT_ID: process.env["NEXT_PUBLIC_OPENAI_PROJECT_ID"],
+    }
+};
 
 export default nextConfig;
