0663e6b8fa31ba45b8c1f60ade66accf7b965106
John Absolu <Johnson.absolu@hotmail.com>
2025-04-20 15:52:11 -0400

    Update env

--- notesage-client/next.config.mjs
+++ notesage-client/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_OPENAI_API_KEY: process.env["NEXT_PUBLIC_OPENAI_API_KEY"],
-        NEXT_PUBLIC_OPENAI_ORG_ID: process.env["NEXT_PUBLIC_OPENAI_ORG_ID"],
-        NEXT_PUBLIC_OPENAI_PROJECT_ID: process.env["NEXT_PUBLIC_OPENAI_PROJECT_ID"],
+        OPENAI_API_KEY: process.env["NEXT_PUBLIC_OPENAI_API_KEY"],
+        OPENAI_ORG_ID: process.env["NEXT_PUBLIC_OPENAI_ORG_ID"],
+        OPENAI_PROJECT_ID: process.env["NEXT_PUBLIC_OPENAI_PROJECT_ID"],
         API_URL: process.env["API_URL"]
     }
 };
